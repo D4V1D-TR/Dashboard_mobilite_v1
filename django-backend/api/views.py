2d9f360580f4843731dde739a76817c9f3aba06f
D4V1D-TR <david.truong@lpa.fr>
2025-02-25 09:57:52 +0100

    LPA&Co api

--- django-backend/api/views.py
+++ django-backend/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 
 from api.api.Velov import Velov
 from api.api.Parking import Parking
+from api.api.LPAandCo import LPAandCo
 
 @api_view(["GET"])
 def velov_data(request):
@@ -17,3 +18,10 @@ def parking_data(request):
     data = Parking(url="https://data.grandlyon.com/fr/datapusher/ws/rdata/pvo_patrimoine_voirie.parkingtr/all.json?maxfeatures=-1&start=1&filename=parkings-de-la-metropole-de-lyon---disponibilites-temps-reel-v2").main(id="truongdavid.pro@gmail.com", mdp="Azerty01?")
     data = data.to_dict(orient="records")
     return Response(data)
+
+@api_view(["GET"])
+def lpaandco_data(request):
+    data = LPAandCo(url="https://data.grandlyon.com/fr/datapusher/ws/rdata/lpa_mobilite.parking_lpaco_2_0_0/all.json?maxfeatures=-1&start=1&filename=parkings-lpa-co-metropole-lyon-v2").main(id="truongdavid.pro@gmail.com", mdp="Azerty01?")
+    data = data.to_dict(orient="records")
+    return Response(data)
+
